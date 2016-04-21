ce23be3ad14c213331d91e8d2261f7159959a4d1
KNIGHTMASTER <fauzi.knigtmaster.achmad@gmail.com>
2016-04-21 16:01:12 +0700

    Initial Commit

---
+++ src/main/java/com/zisal/learn/vaadin/LearnVaadinApplication.java
@@ -0,0 +1,12 @@
+package com.zisal.learn.vaadin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnVaadinApplication extends ServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnVaadinApplication.class, args);
+	}
+}
